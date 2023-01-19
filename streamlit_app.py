3894fc03665421cbe1cd68ea484739deb939aeca
Kedar <116085901+KedarChitrakathi@users.noreply.github.com>
2023-01-19 23:32:24 +0530

    Update streamlit_app.py
    commenting the raw json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 
 # getting the above json data and normalize it
