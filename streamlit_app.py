a97cdafb2fc7a65c646833283f72efc0ced18128
Kedar <116085901+KedarChitrakathi@users.noreply.github.com>
2023-01-19 23:54:09 +0530

    Update streamlit_app.py
    adding a variable to select fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,12 @@ streamlit.dataframe(fruits_to_show)
 # NEW section to display fruityvice api response 
 
 streamlit.header("Fruityvice Fruit Advice!")
+# adding a variable for asking a fruit choice
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # getting the above json data and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
