c4018e5ef22024cd3ba069a8b2ff6d566e179934
Kedar <116085901+KedarChitrakathi@users.noreply.github.com>
2023-01-26 21:13:16 +0530

    Update streamlit_app.py
    Just adding a error demo code for inserting into snowflake through snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,6 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice_2 = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', fruit_choice_2)
+
+# this will not work currently , but just adding as per guidance
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
