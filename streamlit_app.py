5d76c91d228171063cb2c692d96d80996e813a77
Kedar <116085901+KedarChitrakathi@users.noreply.github.com>
2023-01-26 21:30:43 +0530

    Update streamlit_app.py
    adding structure of fruityvice in if else block

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,16 +35,19 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 # adding a variable for asking a fruit choice
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+       streamlit.error("Please select a fruit to get information")
+    else:
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       streamlit.dataframe(fruityvice_normalized)
+#streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except URLError as e:
+  streamlit.error()
 
-# getting the above json data and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# putput in the screen as table
-streamlit.dataframe(fruityvice_normalized)
 
 #don't run anything past here , while we troubleshoot
 streamlit.stop()
